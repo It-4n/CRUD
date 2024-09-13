f00d0ad5eb0b8b01d33abf0dd74b83afdc04b2f6
It-4n <128805445+It-4n@users.noreply.github.com>
2024-09-13 10:42:10 -0300

    ATT

--- app.js
+++ app.js
@@ -25,8 +25,12 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.get('/login', (req, res) => {
-    res.render('login');
+app.get('/userLogin', (req, res) => {
+    res.render('userLogin');
+});
+
+app.get('/userRegister', (req,res) => {
+    res.render('userRegister');
 });
 
 app.get('/addBook', (req, res) => {
--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('library', 'root', 'cimatec', {
+const sequelize = new Sequelize('library', 'root', '1234', {
     dialect: 'mysql',
     host: 'localhost',
     port: '3306'
